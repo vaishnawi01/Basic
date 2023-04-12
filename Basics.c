8a21de24b18a8939032c4ba6839d91edcc0f80a2
vaishnawi01 <125429485+vaishnawi01@users.noreply.github.com>
2023-04-12 10:38:08 +0530

    Add files via upload

---
+++ Basics.c
@@ -0,0 +1,20 @@
+// Basics of C 
+#include <stdio.h>
+int main()
+{
+	char name[]="SYMBIOSIS";
+	printf("%s \n",name);
+	
+	int i=10;
+	printf("%d \n",i);
+	
+	float f=8.9;
+	printf("%f \n",f);
+	
+	int a;
+	printf("Enter: ");
+	scanf("%d",&a);
+	printf("%d",a);
+	
+	return 0;
+}
